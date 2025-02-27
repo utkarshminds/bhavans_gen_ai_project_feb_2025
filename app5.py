7a25f9d7b4b34ee7250c3cfbd950fd4ef969bc86
utkarshminds <160325202+utkarshminds@users.noreply.github.com>
2025-02-27 14:46:24 +0530

    Update app5.py

--- app5.py
+++ app5.py
@@ -9,7 +9,7 @@ Steps
 import streamlit as st
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyC4O_3vLpw-LVC0yikwjiA4UceuU0pRpOw")
+genai.configure(api_key=st.secrets['GOOGLE_API_KEY']['GOOGLE_API_KEY'])
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
